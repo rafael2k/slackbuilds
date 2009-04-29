1d3107c4da15be40c7a4d08cfa744df8e736fdb2
rhatto <rhatto@370017ae-e619-0410-ac65-c121f96126d4>
2009-04-29 12:55:09 +0000

    EOLing old patches
    git-svn-id: svn+slack://slack.fluxo.info/var/svn/slackbuilds@2175 370017ae-e619-0410-ac65-c121f96126d4

--- patches/mozilla-firefox/doinst.sh
+++
@@ -1,7 +0,0 @@
-
-FIREFOXDIR=usr/lib64/firefox-VERSION
-REGXPCOM=../../../$FIREFOXDIR/regxpcom
-FIREFOX=../../../$FIREFOXDIR/firefox-bin
-
-# Register the default extensions
-( cd $FIREFOXDIR && ./run-mozilla.sh $REGXPCOM && ./run-mozilla.sh $FIREFOX -register )
